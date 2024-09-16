e99d3537e25a41df3fa66fa57da505cdc6c1657a
Paul-vasile Dominan <pauldmnn@gmail.com>
2024-09-16 09:10:56 +0000

    initial commit

---
+++ js/myscript.js
@@ -0,0 +1,11 @@
+// $(document).ready(function() {
+//     $("h2").addClass("underline"); // underlines all <h2> elements
+//     $("ul").addClass("border"); // adds border to all list items
+//     $("ul").removeClass("border"); // removes the class 
+    
+// });
+
+$(document).ready(function() {
+ 
+});
+    
